b2f75f10d269e3394f075ee9a22efbee496b7086
Ilya Natarius <ilyanatarius@Ilyas-MacBook-Pro.local>
2018-02-19 13:18:44 -0600

    added validation of user data, cleaned up some functions, removed generic db functions

--- crimemap.py
+++ crimemap.py
@@ -1,7 +1,10 @@
+import dateparser
+import datetime
 from flask import Flask
 from flask import render_template
 from flask import request
 import json
+import string
 
 import dbconfig
 if dbconfig.test:
@@ -11,39 +14,42 @@ else:
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['mugging', 
+              'break-in']
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = request.form.get("latitude")
-    longtitude = request.form.get("longtitude")
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if note date:
+        return home("Invalide date. Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longtitude = float(request.form.get("longtitude"))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longtitude, description)
     return home()
 
-@app.route("/add", methods=["POST"])
-def add():
+def format_date(userdate):
+    date = dateparser.parse(userdate)
     try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print e
-    return home()
+        return datetime.datetime.strftime(date, "&Y-%m-%d")
+    except TypeError:
+        return None
 
-@app.route("/clear")
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print e
-    return home()
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
